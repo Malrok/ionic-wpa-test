adb983141958d0b9e7ac46fd4fcea5a2d0ebbd6a
Simon Bernardin <simon.bernardin@moventes.com>
2018-12-04 15:04:23 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 import { TranslateService } from '@ngx-translate/core';
-import { Config, Nav, Platform } from 'ionic-angular';
-
+import { Config, Nav } from 'ionic-angular';
 import { FirstRunPage } from '../pages';
-import { Settings } from '../providers';
+
 
 @Component({
   template: `<ion-menu [content]="content">
@@ -45,13 +42,13 @@ export class MyApp {
     { title: 'Search', component: 'SearchPage' }
   ]
 
-  constructor(private translate: TranslateService, platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+  constructor(private translate: TranslateService, private config: Config) {
+    // platform.ready().then(() => {
+    //   // Okay, so the platform is ready and our plugins are available.
+    //   // Here you can do any higher level native things you might need.
+    //   this.statusBar.styleDefault();
+    //   this.splashScreen.hide();
+    // });
     this.initTranslate();
   }
 
